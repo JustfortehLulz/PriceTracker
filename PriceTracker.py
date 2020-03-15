e1a7dffb0cfdd4cfa6eb917e326128bc2fbab923
JustfortehLulz <32005739+JustfortehLulz@users.noreply.github.com>
2020-03-14 21:48:19 -0700

    Update PriceTracker.py
    Ability for user to select own products and code will search and return product names and prices

--- PriceTracker.py
+++ PriceTracker.py
@@ -2,27 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 #import sqlite3
 
+# skip sponsored products 
+
+ok = []
+
+productName = input("what item: ")
+
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36'}
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+title = productName
 
 # CAN websites
 
 ## For Amazon website
-driver = webdriver.Chrome()
-driver.maximize_window()
+
 driver.get("https://www.amazon.ca/")
-
-productName = input("what item: ")
-
-title = productName
 
 text = driver.find_element_by_id("twotabsearchtextbox")
 text.send_keys(productName)
 text.send_keys(Keys.RETURN)
-
-button = driver.find_element_by_xpath("//a[@class='a-link-normal a-text-normal']")
-button.click()
 
 amazonLink = driver.current_url
 
@@ -31,9 +35,6 @@ driver.get("https://www.canadacomputers.com/index.php")
 text = driver.find_element_by_id("cc_quick_search")
 text.send_keys(productName)
 text.send_keys(Keys.RETURN)
-
-button = driver.find_element_by_xpath("//a[@class='text-dark text-truncate_3']")
-button.click()
 
 ccLink = driver.current_url
 
@@ -44,8 +45,8 @@ text = driver.find_element_by_name("Search")
 text.send_keys(productName)
 text.send_keys(Keys.RETURN)
 
-button = driver.find_element_by_xpath("//div[@class='c-shca-icon-item__body-name']")
-button.click()
+#button = driver.find_element_by_xpath("//div[@class='c-shca-icon-item__body-name']")
+#button.click()
 
 meLink = driver.current_url
 ## Amazon link
@@ -60,86 +61,71 @@ print("Amazon price")
 
 page = requests.get(amazonLink, headers=headers)
 soup = BeautifulSoup(page.content, 'html.parser')
-priceString = soup.find(id="priceblock_ourprice").get_text()
-priceVal = float(priceString[5:])
+for elem in soup.find_all("div", {"class" : "s-include-content-margin s-border-bottom"}):
+	product = elem.find("a" , {"class" :  "a-link-normal a-text-normal"})
+	productName = product.find("span" , {"class" : "a-size-medium a-color-base a-text-normal"})
+	priceWhole = elem.find("span" , {"class" : "a-offscreen"}).get_text().strip()
+	# priceFraction = elem.find("span" , {"class" : "a-price-fraction"}).get_text().strip()
+	print(productName.get_text().strip())
+	print(priceWhole)
 
-print(title)
-print(priceVal)
 print()
+
 
 print("Canada Computers Prices")
 
 page = requests.get(ccLink, headers=headers)
 soup = BeautifulSoup(page.content, 'html.parser')
-priceString = soup.find("span", {"class" : "h2-big"}).get_text()
-priceString = priceString[2:-1]
 
-print(title)
-print(priceString)
+number = 1
+for elem in soup.find_all("div" , {"class" : "px-0 col-12 productInfoSearch pt-2"}):
+	productName = elem.find("span" , {"class" : "text-dark d-block productTemplate_title"}).get_text().strip()
+	price = elem.find("span" , {"class" : "d-block mb-0 pq-hdr-product_price line-height"})
+	if (price == None):
+		price = elem.find("span" , {"class" : "text-danger d-block mb-0 pq-hdr-product_price line-height"})
+	print(productName)
+	print(price.get_text().strip())
+	# productName = elem.find("span" , {"class" : "text-dark d-block productTemplate_title"}).get_text().strip()
+	# price = elem.find("span" , {"class" : "d-block mb-0 pq-hdr-product_price line-height"}).get_text().strip()
+	# print(productName)
+	# print(price)
+# titleCC = soup.find("h1", {"class" : "h3 product-title mb-2"}).get_text().strip()
+# priceString = soup.find("span", {"class" : "h2-big"}).get_text()
+# priceString = priceString[2:-1]
+
+#print(titleCC)
+#print(priceString)
 print()
+############################################################# Memory Express
+
+productsME = []
+pricesME = []
 
 print("Memory Express Prices")
 
 page = requests.get(meLink,headers=headers)
 soup = BeautifulSoup(page.content,'html.parser')
-price = soup.find("div", {"class" : "GrandTotal c-capr-pricing__grand-total"}).get_text()
-price = price[6:]
-print(title)
-print(price)
-print()
 
+### grabs the entire list of products with similar name
+### selects a number 
+# for elem in soup.find_all("div",{"class" : "c-shca-icon-item__body-name"}):
+# 	print(elem.find('a')['href'])
+# 	print(str(number) + ": " + elem.find('a').get_text().strip())
 
-# for url in amazon:
-#     page = requests.get(url, headers=headers)
+# for elem in soup.find_all("div", {"class": "c-shca-icon-item__summary-list"}):
+# 	print(elem.find("span").get_text().strip())
+number = 1
+for elem in soup.find_all("div" , {"class" : "c-shca-icon-item"}):
+	#print(elem.find("span").get_text().strip())
+	productName = elem.find("div" , {"class" : "c-shca-icon-item__body-name"})
+	
+	priceElem = elem.find("div" , {"class" : "c-shca-icon-item__summary"})
+	price = priceElem.find("div", {"class": "c-shca-icon-item__summary-list"})
+	print(str(number) + ": " + productName.find('a').get_text().strip() + " " + price.find("span").get_text().strip())
+	print()
+	number += 1
 
-#     soup = BeautifulSoup(page.content, 'html.parser')
-#     title = soup.find(id="productTitle").get_text()
-#     priceString = soup.find(id="priceblock_ourprice").get_text()
-#     priceVal = float(priceString[5:])
-
-#     amazonPrices[title.strip()] = priceVal
-#     print(title.strip())
-#     print(priceString[5:])
-# print("END OF AMAZON PRICES\n")
-
-# print("CANADA COMPUTERS PRICES\n")
-# ### canada computers
-# CC = ['https://www.canadacomputers.com/product_info.php?cPath=4_64&item_id=120458','https://www.canadacomputers.com/product_info.php?cPath=26_1832_1833&item_id=140922','https://www.canadacomputers.com/product_info.php?cPath=24_311_1326&item_id=096846'
-#       'https://www.canadacomputers.com/product_info.php?cPath=179_1927_1928&item_id=120759','https://www.canadacomputers.com/product_info.php?cPath=15_1086_210&item_id=132494','https://www.canadacomputers.com/product_info.php?cPath=43_557_559&item_id=137857',
-#       'https://www.canadacomputers.com/product_info.php?cPath=6_112&item_id=108444']
-
-# for url in CC:
-#     page = requests.get(url, headers=headers)
-#     soup = BeautifulSoup(page.content,'html.parser')
-#     title = soup.find("h1", {"class" : "h3 product-title mb-2"}).get_text()
-#     price = soup.find("span", {"class" : "h2-big"}).get_text()
-
-#     canadaComputersPrices[title.strip()] = float(price[2:-1])
-#     print(title.strip())
-#     print(price)
-# print("END OF CANADA COMPUTER PRICES\n")
-
-# ### memory express
-# ME = ['https://www.memoryexpress.com/Products/MX78787','https://www.memoryexpress.com/Products/MX61227','https://www.memoryexpress.com/Products/MX76126','https://www.memoryexpress.com/Products/MX77286','https://www.memoryexpress.com/Products/MX70610',
-#       'https://ca.pcpartpicker.com/mr/memoryexpress/cXg323']
-
-# print("MEMORY EXPRESS PRICES\n")
-# for url in ME:
-#     page = requests.get(url,headers=headers)
-#     soup = BeautifulSoup(page.content,'html.parser')
-#     title = soup.find("h1").get_text()
-#     price = soup.find("div", {"class" : "GrandTotal c-capr-pricing__grand-total"}).get_text()
-
-#     memoryExpressPrices[title.strip()] = price[7:-15]
-#     print(title.strip())
-#     print(price[6:])
-# print("END OF MEMORY EXPRESS PRICES\n")
-
-# print(amazonPrices)
-# print('\n')
-# print(canadaComputersPrices)
-# print('\n')
-# print(memoryExpressPrices)
+driver.quit()
 
 
 ### insert values into database
